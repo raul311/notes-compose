7d56f90e955f1862c4f7c9053ae3604ea4d92ab8
raul311 <raul311@gmail.com>
2021-09-26 20:45:30 -0700

    Updated Note dataclass to follow DB clases, implemented correct functionality to repository class

--- app/src/main/java/com/raul311/notescompose/models/Note.kt
+++ app/src/main/java/com/raul311/notescompose/models/Note.kt
@@ -1,31 +1,34 @@
 package com.raul311.notescompose.models
 
-import androidx.compose.runtime.Immutable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Immutable
+@Entity
 data class Note(
-    val id: Long,
-    val title: String,
-    val data: String = ""
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    @ColumnInfo(name = "title") val title: String?,
+    @ColumnInfo(name = "body") val data: String = "",
+    @ColumnInfo(name = "version") val version: Int
 )
 
-val notes = listOf(
-    Note(
-        id = 1L,
-        title = "Title to first note",
-        data = "This is the body of the note section"
-    ),
-    Note(
-        id = 2L,
-        title = "My Title",
-        data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
-                "sed do eiusmod tempor incididunt ut labore et dolore magna " +
-                "aliqua. Ut enim ad minim veniam, quis nostrud exercitation " +
-                "ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    ),
-    Note(
-        id = 3L,
-        title = "Title extra note",
-        data = "This is the body of the note section"
-    )
-)
+//val notes = listOf(
+//    Note(
+//        id = 1,
+//        title = "Title to first note",
+//        data = "This is the body of the note section"
+//    ),
+//    Note(
+//        id = 2,
+//        title = "My Title",
+//        data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+//                "sed do eiusmod tempor incididunt ut labore et dolore magna " +
+//                "aliqua. Ut enim ad minim veniam, quis nostrud exercitation " +
+//                "ullamco laboris nisi ut aliquip ex ea commodo consequat."
+//    ),
+//    Note(
+//        id = 3,
+//        title = "Title extra note",
+//        data = "This is the body of the note section"
+//    )
+//)
--- app/src/main/java/com/raul311/notescompose/models/NotesRepo.kt
+++ app/src/main/java/com/raul311/notescompose/models/NotesRepo.kt
@@ -1,8 +1,17 @@
 package com.raul311.notescompose.models
 
-/**
- * A fake repo
- */
-object NotesRepo {
-    fun getNotes(): List<Note> = notes
+import androidx.lifecycle.LiveData
+
+class NotesRepo(private val noteDao: NoteDao) {
+
+    fun getAllNotes(): LiveData<List<Note>> = noteDao.getNotes()
+
+    fun getNote(id: Long): LiveData<Note> = noteDao.getNote(id)
+
+    fun insertNode(note: Note) = noteDao.insertNode(note)
+
+    fun updateNote(note: Note) = noteDao.updateNote(note)
+
+    fun deleteNote(note: Note) = noteDao.deleteNote(note)
+
 }
